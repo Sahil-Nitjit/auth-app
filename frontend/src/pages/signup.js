7de39f073dfb2deb0f0083f38bbac523c2629fda
Sahil Yadav <sahil.it.22@nitj.ac.in>
2025-04-20 22:58:42 +0530

    Initial commit

---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { HandleError, HandleSuccess } from '../utils';
+
+function Signup() {
+  const [signupInfo, setSignupInfo] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSignupInfo((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    const { name, email, password } = signupInfo;
+    if (!name || !email || !password) {
+      return HandleError('Name, email, and password are required');
+    }
+
+    try {
+      const url = `https://deploy-mern-app-1-api.vercel.app/auth/signup`;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signupInfo)
+      });
+
+      const result = await response.json();
+      const { success, message, error } = result;
+
+      if (success) {
+        HandleSuccess(message);
+        setTimeout(() => navigate('/login'), 1000);
+      } else if (error) {
+        const details = error?.details?.[0]?.message || error.message;
+        HandleError(details);
+      } else {
+        HandleError(message);
+      }
+    } catch (err) {
+      HandleError(err.message || err);
+    }
+  };
+
+  return (
+    <div className='container'>
+      <h1> SignUp </h1>
+      <form onSubmit={handleSignup}>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input
+            onChange={handleChange}
+            type='text'
+            name='name'
+            autoFocus
+            placeholder='Enter your name...'
+            value={signupInfo.name}
+          />
+        </div>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input
+            onChange={handleChange}
+            type='email'
+            name='email'
+            placeholder='Enter your email...'
+            value={signupInfo.email}
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input
+            onChange={handleChange}
+            type='password'
+            name='password'
+            placeholder='Enter your password...'
+            value={signupInfo.password}
+          />
+        </div>
+        <button type='submit'>Signup</button>
+        <span>
+          Already have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default Signup;
